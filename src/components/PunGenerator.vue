bdcabce9c1c76b4ed6d91a34f462a02c87ee6776
Amanda Harris <alece.fwb@gmail.com>
2020-11-11 19:34:43 -0600

    starting work on pungenerator

--- src/components/PunGenerator.vue
+++ src/components/PunGenerator.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Random Pun Generator</h1>
+  <div>
+    <h1>Random Pun Generator</h1>
+    <!-- TODO: Add pun into UI -->
+    <button onclick="randomPun()">Randomize</button>
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class PunGenerator extends Vue {
+  @Prop() private msg!: string;
+}
+
+const randomPun = function() {
+  // TODO: Add puns
+  const pun1 = "I knew a woman who owned a taser. Man, was she stunning!";
+  return pun1;
+};
+</script>
+
+<style scoped></style>
