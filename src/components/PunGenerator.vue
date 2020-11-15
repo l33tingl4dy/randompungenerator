79940e8b8633f0f13fecdc2a6fa64af7655faf6f
Amanda Harris <alece.fwb@gmail.com>
2020-11-14 18:20:49 -0600

    added randomizer for puns

--- src/components/PunGenerator.vue
+++ src/components/PunGenerator.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <h1>Random Pun Generator</h1>
-    <!-- TODO: Add pun into UI -->
     <span class="pun">{{ pun }}</span
     ><br />
     <button class="random-button" onclick="randomPun()">Randomize</button>
@@ -14,7 +13,13 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class PunGenerator extends Vue {
   @Prop() private msg!: string;
-  public pun = "I knew a woman who owned a taser. Man, was she stunning!";
+  public puns = [
+    "I knew a woman who owned a taser. Man, was she stunning!",
+    "I lost my job at the bank on my very first day. A woman asked me to check her balance, so I pushed her over",
+    "It's hard to explain puns to kleptomaniacs because they always take things literally",
+    "Two windmills are standing in a wind farm. One asks, “What’s your favorite kind of music?” The other says, “I’m a big metal fan.”"
+  ];
+  public pun = this.puns[Math.floor(Math.random() * this.puns.length)];
   randomPun = function() {
     // TODO: Add puns
   };
@@ -26,6 +31,7 @@ export default class PunGenerator extends Vue {
   width: 100%;
   justify-content: center;
   font-size: 35px;
+  padding: 10%;
 }
 .random-button {
   margin: 40px;
